347311aae9520e71311f3d874828025f42bd7a3c
maria <cristinabelen97@gmail.com>
2023-10-03 15:36:36 -0500

    Add map

--- src/components/SearchMap/index.jsx
+++ src/components/SearchMap/index.jsx
@@ -87,7 +87,7 @@ export default function SearchMap() {
         ))}
       </div>
       <div className='div-map'>
-        {/* <MapContainers positions={positions} /> */}
+        <MapContainers positions={positions} />
       </div>
     </section>
 
